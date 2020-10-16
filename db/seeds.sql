46d226a74ead50793fb4c2e474130091fd24261f
Drew A <alvarez.drew@gmail.com>
2020-10-16 17:50:02 -0500

    scheema and seeds sql started, got requires and exports laid out

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Double Bacon Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Veggie Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Hawaiian Burger', true);
