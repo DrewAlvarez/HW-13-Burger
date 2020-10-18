236e13beab5ed77ed8951b82a4bc82412d86d070
Drew A <alvarez.drew@gmail.com>
2020-10-18 09:10:08 -0500

    controller.js, burger.js and index worked on

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -2,6 +2,17 @@ var express = require("express");
 
 var router = express.Router();
 
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+        res.render("index", { burgers: data });
+    });
+});
+
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne(req.body.burger_name, function(result) {
+        res.json({ id: result.insertId });
+    });
+});
 module.exports = router;
--- models/burger.js
+++ models/burger.js
@@ -1,4 +1,20 @@
 var orm = require("../config/orm.js");
 
-var burger = {};
+var burger = {
+    selectAll: function(cb) {
+        orm.selectAll("burgers", function(res) {
+            cb(res);
+        });
+    },
+    insertOne: function(col, cb) {
+        orm.insertOne(col, function(res) {
+            cb(res);
+        });
+    },
+    updateOne: function(value, cb) {
+        orm.update(value, function(res) {
+            cb(res);
+        });
+    }
+};
 module.exports = burger;
